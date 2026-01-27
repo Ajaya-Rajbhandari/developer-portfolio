4d1ede4769d76e86886507d8826f6f57b5b59164
Ajaya-Rajbhandari <ajaya.shrestha.521@gmail.com>
2026-01-27 13:19:02 +0545

    Add meta fields to personal data fallback for TypeScript compatibility

--- src/utils/data/personal-data.ts
+++ src/utils/data/personal-data.ts
@@ -4,6 +4,9 @@ export const personalData = {
   designation: "Software Developer",
   description:
     "My name is Ajaya Rajbhandari. I am a Intermediate Programmer. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
+  metaTitle: "Ajaya Rajbhandari - Developer Portfolio",
+  metaDescription: "Portfolio of Ajaya Rajbhandari, a Full-Stack Software Developer.",
+  ogImage: "/profile.png",
   email: "ajaya.shrestha.521@gmail.com",
   phone: "+9779817388966",
   address: "Belbari, Morang, Nepal ",

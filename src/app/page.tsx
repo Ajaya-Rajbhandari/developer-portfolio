147fcefd65fba4c8713a90b3b0997a399a5c3b3e
Ajaya-Rajbhandari <ajaya.shrestha.521@gmail.com>
2026-01-27 11:50:41 +0545

    Add periodic ISR for Sanity content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import BentoGridWrapper from "@/components/home/bento-grid-wrapper";
 import Footer from "@/components/footer/footer";
 
+// Revalidate Sanity content periodically so production picks up new entries
+export const revalidate = 60;
+
 export default async function Home() {
   return (
     <>
